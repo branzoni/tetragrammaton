10b5540a2114a24ebd95028352e35979f9bc38f3
branzoni <sergey.v.afanasyev@gmail.com>
2023-01-27 13:24:13 +0300

    delete all

--- src/HTTP/Header.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Tet\HTTP;
-
-class Header{
-    public string $name;
-    public string $value;
-}
--- src/Routing/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Tet\Routing;
-
-class Routes extends \ArrayObject
-{
-}
