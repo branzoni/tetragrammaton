62a4b9725edcb96bfaa672eaf754a7a9a7ebe904
branzoni <sergey.v.afanasyev@gmail.com>
2024-06-29 15:25:51 +0300

    Update Mailer.php

--- Src/Mail/Mailer.php
+++ Src/Mail/Mailer.php
@@ -2,65 +2,42 @@
 
 namespace Tet\Mail;
 
-use Tet\Mail\Message;
-
 class Mailer
 {
-    public static string $smtpHostname;
-    public static int $smtpPort;
-    public static int $smtpTimeout; // в миллисекундах
-    public static string $smtpLogin;
-    public static string $smtpPassword;
+    public static string $hostname;
+    public static int $port;
+    public static int $timeout; // в миллисекундах
+    public static string $login;
+    public static string $password;
 
-    public static string $messageTo;
-    public static string $messageFrom;
-    public static string $messageSender;
-    public static string $messageReplyTo = '';
-    public static string $messageText;
-    public static string $messageSubject;
-    public static array $messageAttachments;
-    public static string $messageAsHTML;
+	public function setSMTP($host, $port, $timeout, $login, $password) {
+		self::$hostname = $host;
+		self::$port = $port;
+		self::$timeout = $timeout;
+		self::$login = $login;
+		self::$password = $password;
+	}
 
-    static function send(): void
+    public static function send(\Tet\Mail\Message $message): void
     {
-        $message = self::createMessage();
+//        $message = new Message(
+//			self::$messageSubject,
+//			self::$messageText,
+//			self::$messageAttachments,
+//			self::$messageTo,
+//			self::$messageFrom,
+//			self::$messageSender,
+//			self::$messageReplyTo,
+//			self::$messageAsHTML
+//		);
 
-        $smtp = self::createSMTP();
-        $smtp->connect();
+		if (!self::$hostname || !self::$port || !self::$login || !self::$password || self::$timeout) {
+			throw new \Exception('Mailer requires hostname, port, login, password, timeout');
+		}
 
-        $smtp->sendHELO("MyMail");
-        $smtp->sendSTARTTLS();
-        $smtp->sendHELO("MyMail");
-        $smtp->sendAUTHLOGIN();
-        $smtp->sendCommand(base64_encode(self::$smtpLogin));
-        $smtp->sendCommand(base64_encode(self::$smtpPassword));
-        $smtp->sendMAILFROM(self::$messageFrom);
-        $smtp->sendRCPTTO(self::$messageTo);
-        $smtp->sendDATA($message->output());
-        $smtp->sendQUIT();
-    }
-
-    private static function createMessage(): Message
-    {
-        $message = new Message;
-        $message->to = self::$messageTo;
-        $message->subject = self::$messageSubject;
-        $message->text = self::$messageText;
-        $message->attachments = self::$messageAttachments;
-        $message->from = self::$messageFrom;
-        $message->sender = self::$messageSender;
-        $message->html = self::$messageAsHTML;
-        $message->reply_to = self::$messageReplyTo;
-        return $message;
-    }
-
-
-    private static function createSMTP(): SMTP
-    {
         $smtp = new SMTP;
-        $smtp->hostname = self::$smtpHostname;
-        $smtp->port = self::$smtpPort;
-        $smtp->timeout = self::$smtpTimeout;
-        return $smtp;
+        $smtp->connect(self::$hostname, self::$port, self::$timeout);
+		$smtp->setLoginAndPassword(self::$login, self::$password);
+		$smtp->sendMessageAndQuit($message);
     }
 }
