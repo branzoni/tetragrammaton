835d85ae902fb9465cb3e9c7400e3b08952013ae
branzoni <sergey.v.afanasyev@gmail.com>
2024-06-29 14:29:10 +0300

    Update Socket.php

--- Src/Mail/Socket.php
+++ Src/Mail/Socket.php
@@ -4,43 +4,26 @@ namespace Tet\Mail;
 
 class Socket
 {
-
-	public string $hostname;
-	public int $port;
-	public int $timeout; // в миллисекундах
 	private $handle;
-	public bool $debug = false;
+	private bool $debug = false;
 
-	function open(): void
+	public function open(string $hostname, int $port, int $timeout, $debug = false): void
 	{
-		if (!$this->hostname) throw new \Exception("Socket Object: hostname not set");
-		if (!$this->port) throw new \Exception("Socket Object: port not set");
-		if (!$this->timeout) throw new \Exception("Socket Object: timeout not set");
+		$this->debug = $debug;
+		$this->handle = fsockopen($hostname, $port, $errorCode, $errorMessage, $timeout / 1000);
+		if (!$this->handle) throw new \Exception("Socket opening failure: $errorMessage", $errorCode);
 
-		$this->handle = fsockopen($this->hostname, $this->port, $errno, $errstr, $this->timeout / 1000);
-		if (!$this->handle) throw new \Exception("Socket Object: handle creating failure");
-
-		socket_set_timeout($this->handle, 0, $this->timeout * 1000);
+		socket_set_timeout($this->handle, 0, $timeout * 1000);
 	}
 
-	private function echo(string $message)
-	{
-		echo Date('d-m-Y h:i:s') . "$message\r\n";
-	}
-
-	function isOpened(): bool
-	{
-		return boolval($this->handle);
-	}
-
-	function write(string $data): void
+	public function write(string $data): void
 	{
 		if (!$this->isOpened()) throw new \Exception("Socket Object: socket not opened");;
 		if ($this->debug) $this->echo(" - WRITE: $data");
 		fputs($this->handle, $data);
 	}
 
-	function read(): string
+	public function read(): string
 	{
 		if (!$this->isOpened()) throw new \Exception("Socket Object: socket not opened");;
 
@@ -49,18 +32,28 @@ class Socket
 			$data .= $buffer;
 		}
 
-		if ($this->debug) $this->echo(" - READ: $data\r\n");
+		if ($this->debug) $this->echo(" - READ: $data");
 		return $data;
 	}
 
-	function writeAndRead(string $data): ?string
+	public function writeAndRead(string $data): ?string
 	{
 		$this->write($data);
 		return $this->read();
 	}
 
-	function enableCrypto($method = STREAM_CRYPTO_METHOD_TLS_CLIENT): bool
+	public function isOpened(): bool
+	{
+		return boolval($this->handle);
+	}
+
+	public function enableCrypto($method = STREAM_CRYPTO_METHOD_TLS_CLIENT): bool
 	{
 		return stream_socket_enable_crypto($this->handle, true, $method);
+	}
+
+	private function echo(string $message)
+	{
+		echo Date('d-m-Y h:i:s') . "$message\r\n";
 	}
 }
